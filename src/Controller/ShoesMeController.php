3bd51b2d53b6dc97fba6a667903bcdd6fc2851fa
konomoha <konomoha@hotmail.fr>
2021-12-16 14:16:50 +0100

    première version

---
+++ src/Controller/ShoesMeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShoesMeController extends AbstractController
+{
+    #[Route('/shoes/me', name: 'shoes_me')]
+    public function index(): Response
+    {
+        return $this->render('shoes_me/index.html.twig', [
+            'controller_name' => 'ShoesMeController',
+        ]);
+    }
+}
