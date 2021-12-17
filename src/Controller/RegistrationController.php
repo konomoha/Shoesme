4cdf765c98f86c7b8d7fa5e11f9b6f9dc93fd6fd
konomoha <konomoha@hotmail.fr>
2021-12-17 11:55:15 +0100

    maj mohamed 1712

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,7 +18,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $user = new User();
-        $registerForm = $this->createForm(RegistrationFormType::class, $user);
+        $registerForm = $this->createForm(RegistrationFormType::class, $user, ['userRegister'=>true]);
         $registerForm->handleRequest($request);
 
         if ($registerForm->isSubmitted()&& $registerForm->isValid()) 
@@ -66,7 +66,7 @@ class RegistrationController extends AbstractController
     ///////////////////////////CREATION D'UNE ROUTE + TEMPLATE PROFIL (profil.html.twig)\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     #[Route('/profil', name: 'app_profil')]
-    public function userProfil(): Response
+    public function profil(): Response
     {
         
         if(!$this->getUser())
@@ -79,5 +79,58 @@ class RegistrationController extends AbstractController
         return $this->render('registration/profil.html.twig', [
             'profildata'=> $user ]);
 
+    }
+
+    //Route edit mise à part de la route app_profil pour plus de clarté 
+
+    #[Route('/profil/{id}/edit', name: 'app_profil_edit')]
+    public function profilEdit(User $user=null, Request $request, EntityManagerInterface $manager, SluggerInterface $slugger):Response
+    {
+        $profilUpdate = $this->createForm(RegistrationFormType::class, $user, ['userUpdate'=>true]);
+        $profilUpdate->handleRequest($request);
+        
+            $avatarActuel = $user->getAvatar();
+        
+
+        if($profilUpdate->isSubmitted() && $profilUpdate->isValid())
+        {
+            // dd($user);
+            $avatar = $profilUpdate->get('avatar')->getData();
+
+            if($avatar)
+            {   
+                $nomOrigineAvatar = pathinfo($avatar->getClientOriginalName(), PATHINFO_FILENAME);           
+                $secureNomAvatar = $slugger->slug($nomOrigineAvatar);       
+                $nouveauNomFichier = $secureNomAvatar . '-' . uniqid(). '.' .$avatar->guessExtension();
+
+     
+                $avatar->move(
+                    $this->getparameter('avatar_directory'),
+                    $nouveauNomFichier);
+
+                $user->setAvatar($nouveauNomFichier);
+              
+            }
+            
+            else
+            {
+                if(isset($avatarActuel))
+                    $user->setAvatar($avatarActuel);
+
+            else{
+                $user->setAvatar("test");
+            }
+               
+            }
+            
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('success', 'Vous avez modifié vos informations, merci de vous authentifier à nouveau');
+
+            return $this->redirectToRoute('app_logout');
+        }
+        return $this->render('registration/profil_edit.html.twig', [
+            'profilUpdate'=> $profilUpdate->createView(),
+            'avatarActuel'=>$avatarActuel]);
     }
 }
