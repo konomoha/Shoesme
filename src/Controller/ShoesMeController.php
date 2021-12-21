f48e6e01d6357c247495b405547d40d3e6b79af2
konomoha <konomoha@hotmail.fr>
2021-12-21 11:29:39 +0100

    maj moha2112

--- src/Controller/ShoesMeController.php
+++ src/Controller/ShoesMeController.php
@@ -8,6 +8,7 @@ use App\Entity\Chaussure;
 use App\Entity\Commentaire;
 use App\Form\CommentFormType;
 use App\Form\ContactFormType;
+use App\Repository\CommentaireRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -54,7 +55,7 @@ class ShoesMeController extends AbstractController
     }
 
     #[Route('/details_article/{id}', name:'details_article')]
-    public function detailArticle(Request $request, EntityManagerInterface $manager, Chaussure $chaussure):Response
+    public function detailArticle(CommentaireRepository $repoCommentaire, Request $request, EntityManagerInterface $manager, Chaussure $chaussure):Response
     {
         $commentaire = new Commentaire;
         
@@ -73,7 +74,7 @@ class ShoesMeController extends AbstractController
             $commentaire->setChaussure($chaussure);
             $manager->persist($commentaire);
             $manager->flush();
-            $this->addFlash('success', "Félicitations! Votre commentaire a bien été posté!");
+            $this->addFlash('success_comment', "Félicitations! Votre commentaire a bien été posté!");
             return $this->redirectToRoute('details_article', ['id'=> $chaussure->getId()]);
         }
 
@@ -83,6 +84,4 @@ class ShoesMeController extends AbstractController
         ]);
 
     }
-    
 }
-
