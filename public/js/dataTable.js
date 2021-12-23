f19aa6b5995a6ac2f1ed41cb31d5a6426ee4b14c
Langlais78 <elitepaysages@gmail.com>
2021-12-23 16:27:03 +0100

    main comment tableau fr

--- public/js/dataTable.js
+++ public/js/dataTable.js
@@ -43,3 +43,11 @@ $(document).ready(function() {
 });
 
 
+// $(function() {
+//     // Sections height
+//     $(window).resize(function() {
+//         var sH = $(window).height();
+//         $('.NomDeTaClass').css('height', sH + 'px');
+//     });        
+// });
+
