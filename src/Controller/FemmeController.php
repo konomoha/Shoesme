6be86e515574379eb63b92cb8ab0f54dbb05297c
lucasTrswl <lucasturschwell@hotmail.com>
2022-01-04 15:31:45 +0100

    suite

--- src/Controller/FemmeController.php
+++ src/Controller/FemmeController.php
@@ -15,11 +15,11 @@ class FemmeController extends AbstractController
     {
         // ////////////////////////////////////////////////METHODE AFFICHAGE LIMIT /////////////////////////////////////
 
-        $chaussure = $repoChaussure->findAll();
-        // array(), // condition where
-        // array (), //order by
-        // 57, // la limite de chaussures à afficher
-        // 0); // offset
+        $chaussure = $repoChaussure->findBy(
+            array(), // condition where
+            array (), //order by
+            1000, // la limite de chaussures à afficher
+            0); // offset
 
         return $this->render('femme/femme.html.twig', [
             'chaussure'=>$chaussure
